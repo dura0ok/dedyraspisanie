c6ad67643e516e741da44dc1268b4288d96d3743
durachok <fenixrnd@mail.ru>
2022-08-31 16:34:00 +0700

    add click event log

--- app.js
+++ app.js
@@ -2,34 +2,42 @@ console.log(window.location)
 const cells = document.querySelectorAll(".cell")
 const pairs = []
 
-cells.forEach(element=> {
-  let pair = {}
-  const childrens = element.children
+function extractDataFromCell(element) {
+    let pair = {}
+    const childrens = element.children
 
-  const typePairInfo = childrens[0]	
-  const className = ["lek", "pr", "lab"].find(type => typePairInfo.classList.contains(type)) || "undefined"
-  pair["type"] = className
+    const typePairInfo = childrens[0]
+    const className = ["lek", "pr", "lab"].find(type => typePairInfo.classList.contains(type)) || "undefined"
+    pair["type"] = className
 
-  pair["name"] = childrens[1].innerText
-  pair["auditory"] = childrens[2].innerText
+    pair["name"] = childrens[1].innerText
+    pair["auditory"] = childrens[2].innerText
 
-  if(childrens.length > 3){
-  	const tutor = childrens[3]
-  	pair["tutor"] = {"name": tutor.innerText, "href": tutor.href}
+    if (childrens.length > 3) {
+        const tutor = childrens[3]
+        pair["tutor"] = { "name": tutor.innerText, "href": tutor.href }
 
-  }
-  
-  pairs.push(pair)
+    }
 
-  let btn = document.createElement('button')
-  btn.innerHTML = "Delete"
-  btn.classList.add("hide_item_class")
-  element.appendChild(btn)
+    return pair
+
+}
+
+cells.forEach(element => {
+    pairs.push(extractDataFromCell(element))
+
+    let btn = document.createElement('button')
+    btn.innerHTML = "Delete"
+    btn.classList.add("hide_item_class")
+    element.appendChild(btn)
 });
 
-console.log(pairs)
+//console.log(pairs)
 
-const hideButtons = document.querySelectorAll("hide_item_class")
+const hideButtons = document.querySelectorAll(".hide_item_class")
 hideButtons.forEach(btn => {
-  btn.addEventListener('click', () => {console.log("asd")});
+    btn.addEventListener('click', (event) => {
+        const parent = event.target.parentNode
+        console.log(extractDataFromCell(parent))
+    });
 });
